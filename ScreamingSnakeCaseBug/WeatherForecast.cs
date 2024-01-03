6ed0aab7f92777063c1e50b582434490c5b7b9f8
Jacob Ihle <Jacob.Vetle.Kvinnsland.Ihle@akerbp.com>
2024-01-03 14:46:07 +0100

    Added .net6 project that demonstrates bug

---
+++ ScreamingSnakeCaseBug/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ScreamingSnakeCaseBug
+{
+    public class ExampleModel
+    {
+        public DateTime DATE_TIME { get; set; }
+
+        public int TEMPERATURE_CELSIUS { get; set; }
+
+        public int Temperature_Farenheit => 32 + (int)(TEMPERATURE_CELSIUS / 0.5556);
+
+        public string? some_summary { get; set; }
+    }
+}
